41e8c2f534baebfaf5f55c5245f628e0345f1d23
tylerfeldman321 <tylerfeldman321@gmail.com>
2021-10-23 16:47:24 -0400

    Tested and fixed errors; is working now

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import cv2
 from image_preprocessing import *
 from configurations import *
 from detecto.utils import xml_to_csv, read_image
@@ -15,10 +16,10 @@ def create_image_processing_folders(image_preprocessing=no_preprocessing):
     original_files_dir = os.path.join(BASE_DATA_DIR, ORIGINAL_IMAGE_FOLDER)
     original_train = os.path.join(original_files_dir, TRAINING_FOLDER_NAME)
     original_validation = os.path.join(original_files_dir, VALIDATION_FOLDER_NAME)
-    assert os.path.exists(original_train), 'no_preprocessing/train should exist. Add original training images ' \
-                                           'and labels to this file '
-    assert os.path.exists(original_validation), 'no_preprocessing/train should exist. Add original validation ' \
-                                                'images and labels to this file '
+    assert os.path.exists(original_train), f'{original_train} should exist. Add original training images ' \
+                                           'and labels to this directory '
+    assert os.path.exists(original_validation), f'{original_validation} should exist. Add original validation ' \
+                                                'images and labels to this directory'
 
     # Create the directory for the new image preprocessing
     new_image_preprocessing_dir = os.path.join(BASE_DATA_DIR, image_preprocessing.__name__)
@@ -37,22 +38,21 @@ def create_image_processing_folders(image_preprocessing=no_preprocessing):
 
 def copy_images_and_labels(src_dir, dst_dir, image_preprocessing):
     # For each image in the original image training directory, apply the image preprocessing and save into
-    # Assumes
-    images = glob.glob(os.path.join(src_dir, '*.jpg'))
-    labels = glob.glob(os.path.join(src_dir, '.xml'))
+    images = glob.glob(os.path.join(src_dir, '*'+IMAGE_EXT))
+    labels = glob.glob(os.path.join(src_dir, '*'+LABEL_EXT))
     assert len(images) > 0 and len(labels) > 0, f'Could not find images and/or labels to copy over for {src_dir}'
+
+    # Could change the name of the images by appending the preprocessing name to their basename.
+    # In this case we would also have to change the filename field in the corresponding xml
+
     for image in images:
         basename = os.path.basename(image)
-        filename, ext = os.path.splitext(basename)
-        new_filename = filename + image_preprocessing.__name__ + ext
-        save_path = os.path.join(dst_dir, new_filename)
+        save_path = os.path.join(dst_dir, basename)
         img = image_preprocessing(image)
         cv2.imwrite(save_path, img)
     for label in labels:
         basename = os.path.basename(label)
-        filename, ext = os.path.splitext(basename)
-        new_filename = filename + image_preprocessing.__name__ + ext
-        dst = os.path.join(dst_dir, new_filename)
+        dst = os.path.join(dst_dir, basename)
         copyfile(label, dst)
 
 
@@ -106,4 +106,4 @@ def copy_images_and_labels(src_dir, dst_dir, image_preprocessing):
 
 
 if __name__ == '__main__':
-    pass
+    create_image_processing_folders(no_preprocessing)
