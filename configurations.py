78f672ab4d4faeec19811df6c1edbdcdf6ab5c1e
tylerfeldman321 <tylerfeldman321@gmail.com>
2021-10-23 16:44:58 -0400

    Add more default values

--- configurations.py
+++ configurations.py
@@ -10,9 +10,15 @@ ORIGINAL_IMAGE_FOLDER = 'original'
 
 # Values
 DEFAULT_EPOCHS = 50
+DEFAULT_RUNS = 3
+DEFAULT_SEEDS = [0, 1, 2]  # The length of this should be equal to the value of DEFAULT_RUNS
 
 # Paths
 BASE_DATA_DIR = os.path.join('data')
 BASE_OUTPUT_DIR = os.path.join('output')
 TRAINING_FOLDER_NAME = 'train'
 VALIDATION_FOLDER_NAME = 'validation'
+
+# File formats
+IMAGE_EXT = '.jpg'
+LABEL_EXT = '.xml'
