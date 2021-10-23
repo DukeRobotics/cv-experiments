bc12e5805a011f57a0d9aa60d547fa7f2b94e1c5
tylerfeldman321 <tylerfeldman321@gmail.com>
2021-10-23 13:54:51 -0400

    Add code to create new input folders for a given image processing function

--- data.py
+++ data.py
@@ -1,30 +1,108 @@
+import glob
+import os
+from image_preprocessing import *
+from configurations import *
+from detecto.utils import xml_to_csv, read_image
+import torch
+from shutil import copyfile
 
-# This file will handle the datasets for training if needed, not sure right now if we should use this
 
-class CustomDataset:
-    def __init__(self, data_dir, image_preprocessing='no_preprocessing'):
-        self.image_paths = []
-        self.label_paths = []
-        self.training_dataset = None
-        self.validation_dataset = None
-        self.image_preprocessing = image_preprocessing
+def create_image_processing_folders(image_preprocessing=no_preprocessing):
+    assert image_preprocessing in IMAGE_PREPROCESSING_ALGORITHMS, 'image processing technique not added to ' \
+                                                                  'image_preprocessing_algorithms '
 
-        # Search through data_dir and collect image and label paths
-        pass
+    # Check that the original image directories are there
+    original_files_dir = os.path.join(BASE_DATA_DIR, ORIGINAL_IMAGE_FOLDER)
+    original_train = os.path.join(original_files_dir, TRAINING_FOLDER_NAME)
+    original_validation = os.path.join(original_files_dir, VALIDATION_FOLDER_NAME)
+    assert os.path.exists(original_train), 'no_preprocessing/train should exist. Add original training images ' \
+                                           'and labels to this file '
+    assert os.path.exists(original_validation), 'no_preprocessing/train should exist. Add original validation ' \
+                                                'images and labels to this file '
 
-    def __getitem__(self, index):
-        # Read the image
-        # Apply preprocessing step
-        pass
+    # Create the directory for the new image preprocessing
+    new_image_preprocessing_dir = os.path.join(BASE_DATA_DIR, image_preprocessing.__name__)
+    if not os.path.exists(new_image_preprocessing_dir):
+        os.mkdir(new_image_preprocessing_dir)
+    new_train_dir = os.path.join(new_image_preprocessing_dir, TRAINING_FOLDER_NAME)
+    new_validation_dir = os.path.join(new_image_preprocessing_dir, VALIDATION_FOLDER_NAME)
+    if not os.path.exists(new_train_dir):
+        os.mkdir(new_train_dir)
+    if not os.path.exists(new_validation_dir):
+        os.mkdir(new_validation_dir)
 
-    def __len__(self):
-        pass
+    copy_images_and_labels(original_train, new_train_dir, image_preprocessing)
+    copy_images_and_labels(original_validation, new_validation_dir, image_preprocessing)
 
-    def get_training_dataset(self):
-        return self.training_dataset
 
-    def get_validation_dataset(self):
-        return self.validation_dataset
+def copy_images_and_labels(src_dir, dst_dir, image_preprocessing):
+    # For each image in the original image training directory, apply the image preprocessing and save into
+    # Assumes
+    images = glob.glob(os.path.join(src_dir, '*.jpg'))
+    labels = glob.glob(os.path.join(src_dir, '.xml'))
+    assert len(images) > 0 and len(labels) > 0, f'Could not find images and/or labels to copy over for {src_dir}'
+    for image in images:
+        basename = os.path.basename(image)
+        filename, ext = os.path.splitext(basename)
+        new_filename = filename + image_preprocessing.__name__ + ext
+        save_path = os.path.join(dst_dir, new_filename)
+        img = image_preprocessing(image)
+        cv2.imwrite(save_path, img)
+    for label in labels:
+        basename = os.path.basename(label)
+        filename, ext = os.path.splitext(basename)
+        new_filename = filename + image_preprocessing.__name__ + ext
+        dst = os.path.join(dst_dir, new_filename)
+        copyfile(label, dst)
+
+
+# # Will hold off on this approach for now
+# def create_training_and_validation_datasets(data_dir, image_preprocessing, val_split=0.2):
+#     """
+#
+#     :param data_dir: Path to directory where the images and labels are stored. Assuming images are .jpg and labels are
+#     .xml files. Images and corresponding labels should have the same basename. E.g. image1.jpg has label image1.xml
+#     :param image_preprocessing: Type of image preprocessing to use
+#     :param val_split: Proportion of the data to put into the validation set
+#     :return:
+#     """
+#
+#     # Could collect all of the images and labels here
+#     # Then split the data and create two datasets
+#     images = glob.glob(os.path.join(data_dir, '*.jpg'))
+#     labels = glob.glob(os.path.join(data_dir, '*.xml'))
+#
+#     print(images, labels)
+#
+#     return 0
+#
+#
+# # Will hold off on this approach for now
+# class ImagePrepreocessingDataset:
+#     def __init__(self, image_paths, label_paths, image_preprocessing=no_preprocessing):
+#         self.image_paths = []
+#         self.label_paths = []
+#         self.image_preprocessing = image_preprocessing
+#
+#     def __getitem__(self, index):
+#         if torch.is_tensor(index):
+#             index = index.tolist()
+#
+#         # Might want to implement transforms, but for now we'll skip this
+#
+#         # Get the image and label paths
+#         image_path = self.image_paths[index]
+#         label_path = self.label_paths[index]
+#
+#         image = read_image(image_path)
+#
+#         # Apply preprocessing step
+#         image = self.image_preprocessing(image)
+#
+#         return image, targets
+#
+#     def __len__(self):
+#         return len(self.image_paths)
 
 
 if __name__ == '__main__':
