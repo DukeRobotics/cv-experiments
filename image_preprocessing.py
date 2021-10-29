1b2f619ba05cf03a6cac981ff5780bd413a9e5f1
Vedarsh Shah <vedarsh.shah@duke.edu>
2021-10-28 20:00:58 -0400

    Updated tester and added gbrc method

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import math
 from image_preprocessing.DCP import main as DCP
 from image_preprocessing.CLAHE import main as CLAHE
+from image_preprocessing.GBdehazingRCorrection import main as GBRC
 
 # This file will contain all of the different image processing algorithms
 # Each algorithm should have its own separate function
@@ -39,29 +41,55 @@ def clahe(image_path):
     img = cv2.imread(image_path)
     return CLAHE.RecoverCLAHE(img)
 
+# Not working
 def dcp(image_path):
     img = cv2.imread(image_path)
     transmission, sceneRadiance = DCP.getRecoverScene(img)
+    print("past")
 
     # Can return transmission or sceneRadiance
-    return np.uint8(transmission * 255)
-    #return sceneRadiance
+    #return np.uint8(transmission * 255)
+    return sceneRadiance
+
+def gbrc(image_path):
+        img = cv2.imread(image_path)
+        img = (img - img.min()) / (img.max() - img.min()) * 255
+
+        blockSize = 9
+
+        largestDiff = GBRC.determineDepth(img, blockSize)
+        AtomsphericLight, AtomsphericLightGB, AtomsphericLightRGB = GBRC.getAtomsphericLight(largestDiff, img)
+        transmission = GBRC.getTransmission(img, AtomsphericLightRGB, blockSize)
+        transmission = GBRC.refinedtransmission(transmission, img)
+        return np.uint8(transmission[:, :, 0] * 255)
+
+        sceneRadiance_GB = GBRC.sceneRadianceGB(img, transmission, AtomsphericLightRGB)
+        sceneRadiance = GBRC.sceneradiance(img, sceneRadiance_GB)
+        S_x = GBRC.AdaptiveExposureMap(img, sceneRadiance, Lambda=0.3, blockSize=blockSize)
+        sceneRadiance = GBRC.AdaptiveSceneRadiance(sceneRadiance, S_x)
+        
+        # return sceneRadiance
+
 
 if __name__ == '__main__':
     # Test an image processing algorithm
 
-    imgPaths = ["output/CLAHE/train_images/frame216.jpg"]
+    imgPaths = ["test_images/frame216.jpg", "test_images/frame220.jpg", "test_images/frame223.jpg", "test_images/frame230.jpg", "test_images/frame235.jpg", "test_images/frame242.jpg", "test_images/frame258.jpg", ]
     imgs = []
 
     for imgPath in imgPaths:
         imgs.append(no_preprocessing(imgPath))
-        imgs.append(clahe(imgPath))
+        #imgs.append(clahe(imgPath))
         #imgs.append(dcp(imgPath))
+        imgs.append(gbrc(imgPath))
 
-    columns = 2
-    rows = math.ceil(len(imgs) / 2)
-    for i in range(1, columns*rows +1):
-            plt.subplot(rows, columns, i)
-            plt.imshow(cv2.cvtColor(imgs[i-1], cv2.COLOR_BGR2RGB), interpolation="nearest")
-    plt.axis("off")
+    before = True
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    for img, ax in zip(imgs, axes.ravel()):
+        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), interpolation="nearest")
+        ax.xaxis.set_ticks([])
+        ax.yaxis.set_ticks([])
+        ax.set_xlabel("Before" if before else "After")
+        before = not before
+    fig.tight_layout()
     plt.show()
