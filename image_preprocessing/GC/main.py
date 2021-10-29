acd2e16400d0d3885961ab2f0803e980c89bff18
Vedarsh Shah <vedarsh.shah@duke.edu>
2021-10-28 20:47:16 -0400

    Fixed imports in gc

--- image_preprocessing/GC/main.py
+++ image_preprocessing/GC/main.py
@@ -6,7 +6,7 @@ import natsort
 import xlwt
 from skimage import exposure
 
-from GC.sceneRadianceGC import RecoverGC
+from image_preprocessing.GC.sceneRadianceGC import RecoverGC
 
 np.seterr(over='ignore')
 if __name__ == '__main__':
@@ -14,18 +14,18 @@ if __name__ == '__main__':
 folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/GC"
 # folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
 
-path = folder + "/InputImages"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
+# path = folder + "/InputImages"
+# files = os.listdir(path)
+# files =  natsort.natsorted(files)
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        # img = cv2.imread('InputImages/' + file)
-        img = cv2.imread(folder + '/InputImages/' + file)
-        sceneRadiance = RecoverGC(img)
-        print('sceneRadiance',sceneRadiance)
-        cv2.imwrite('OutputImages/' + prefix + '_GC.jpg', sceneRadiance)
+# for i in range(len(files)):
+#     file = files[i]
+#     filepath = path + "/" + file
+#     prefix = file.split('.')[0]
+#     if os.path.isfile(filepath):
+#         print('********    file   ********',file)
+#         # img = cv2.imread('InputImages/' + file)
+#         img = cv2.imread(folder + '/InputImages/' + file)
+#         sceneRadiance = RecoverGC(img)
+#         print('sceneRadiance',sceneRadiance)
+#         cv2.imwrite('OutputImages/' + prefix + '_GC.jpg', sceneRadiance)
