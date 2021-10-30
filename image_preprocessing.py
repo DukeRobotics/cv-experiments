1957280daedd4967526caeb4ea940d74ea14746d
vedarshshah <33162616+vedarshshah@users.noreply.github.com>
2021-10-30 14:55:53 -0400

    Image preprocessing (#3)
    * Updated main to take in preprocessing functions with command line arguments
    * Updated main function to take in image argument, use only no_preprocessing as the default function, and matplotlib displays all generated images in a single figure with function labels

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,31 +1,100 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import argparse, math, time
+from os.path import exists
 from image_preprocessing_pkg.DCP import main as DCP
 from image_preprocessing_pkg.CLAHE import main as CLAHE
 from image_preprocessing_pkg.GBdehazingRCorrection import main as GBRC
 from image_preprocessing_pkg.GC import main as GC
 
-
 # This file will contain all of the different image processing algorithms
 # Each algorithm should have its own separate function
+
+def main():
+    # Use ArgumentParser to get functions and image optional arguments
+    parser = argparse.ArgumentParser(description='Image Preprocessing')
+    parser.add_argument('--functions', type=str, help='An optional argument to specify which image preprocessing functions to run (comma-seperated). If not specified, only no_preprocessing will be run. Ex: "-- functions amine_rhone,clahe,gc" NOTE: no_preprocessing is always run, regardless of the specified functions.')
+    parser.add_argument('--image', type=str, help='An optional argument to specify which image in the test_images folder should be used. If not specified, frame216.jpg will be used.')
+    args = parser.parse_args()
+    
+    # Get list of functions
+    possibles = globals().copy()
+    possibles.update(locals())
+
+    # If the functions argument was specified, check if the function exists and add it to the functions list
+    # If one of the functions does not exist, let user know via command line and stop execution
+    # If the functions argument was not specified, functions list only contains no_preprocessing as the default
+    functionsStr = args.functions
+    functions = [no_preprocessing]
+    if functionsStr :
+        for functionName in functionsStr.split(","):
+            function = possibles.get(functionName)
+            if function:
+                functions.append(function)
+            else:
+                print("There is no image preprocessing function with name " + functionName + ".")
+                return
+    
+    # If the image argument was specified, check if the image exists and replace the value of imgPath with it
+    # If the image does not exist, let the user know via command line and stop execution
+    # If the image argument was not specified, imgPath contains frame216.jpg as the default
+    imgPath = "test_images/frame216.jpg"
+    if args.image:
+        if exists("test_images/" + args.image):
+            imgPath = "test_images/" + args.image
+        else:
+            print("There is no file with name " + args.image + " in the test_images directory.")
+            return
+    
+    # Create subplots a fixed 3 columns and necessary number of rows to display all images
+    cols = 3 if len(functions) >=3 else len(functions)
+    rows = math.ceil(len(functions) / cols)
+    fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6), squeeze=False)
+
+    # Loop through all preprocessing functions that need to be executed
+    # Apply each function to the image, time its executin, and show it on the matplotlib
+    for i in range(len(functions)):
+
+        processingStartTime = time.time()
+        processedImg = functions[i](imgPath)
+        processingTime = time.time() - processingStartTime;
+        print(f'{functions[i].__name__:>20}' + " %.2f secs" % processingTime)
+
+        ax = axes.ravel()[i]
+        ax.imshow(cv2.cvtColor(processedImg, cv2.COLOR_BGR2RGB), interpolation="nearest")
+        ax.xaxis.set_ticks([])
+        ax.yaxis.set_ticks([])
+        ax.set_xlabel(functions[i].__name__)
+
+    # Delete any subplots that do not contain an image
+    for i in range((rows * cols) - len(functions)):
+        fig.delaxes(axes[rows - 1][cols - 1 - i])
+
+    # Show the plot with all the images
+    fig.tight_layout()
+    plt.show()
+    
 
 def no_preprocessing(image_path):
     return cv2.imread(image_path)
 
 
-# Credit to this research paper: http://www.lsis.org/rov3d/article/art_AmineRhone2012.html
+#Credit to this research paper: http://www.lsis.org/rov3d/article/art_AmineRhone2012.html
 def amine_rhone(image_path):
-    # Read Image
+
+    #Read Image
     image = cv2.imread(image_path)
 
-    # Get Color Information
+    #Get Color Information
     blue_min = np.percentile(image[:, :, 0], 4)
     blue_max = np.percentile(image[:, :, 0], 96)
     green_min = np.percentile(image[:, :, 1], 4)
     green_max = np.percentile(image[:, :, 1], 96)
     red_min = np.percentile(image[:, :, 2], 4)
     red_max = np.percentile(image[:, :, 2], 96)
+
 
     # Edit Image
     image = image.astype(np.int64)  # To avoid overflow issues
@@ -80,29 +149,6 @@ def gc(image_path):
     img = cv2.imread(image_path)
     return GC.RecoverGC(img)
 
-
+  
 if __name__ == '__main__':
-    # TODO: Need to clean up this code
-
-    imgPaths = ["test_images/frame216.jpg"]
-    functions = [amine_rhone, clahe, dcp, gbrc, gc]
-    imgs = []
-
-    for imgPath in imgPaths:
-        imgs.append(no_preprocessing(imgPath))
-        # imgs.append(amine_rhone(imgPath))
-        # imgs.append(clahe(imgPath))
-        # imgs.append(dcp(imgPath))
-        # imgs.append(gbrc(imgPath))
-        imgs.append(gc(imgPath))
-
-    before = True
-    fig, axes = plt.subplots(nrows=1, ncols=2)
-    for img, ax in zip(imgs, axes.ravel()):
-        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), interpolation="nearest")
-        ax.xaxis.set_ticks([])
-        ax.yaxis.set_ticks([])
-        ax.set_xlabel("Before" if before else "After")
-        before = not before
-    fig.tight_layout()
-    plt.show()
+    main()
