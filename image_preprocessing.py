07e647cd09cfa252d2270796fb8ad3c7c707fbf4
Vedarsh Shah <vedarsh.shah@duke.edu>
2021-10-30 13:46:23 -0400

    Updated main to take in preprocessing functions with command line arguments

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import math
+import argparse
 from image_preprocessing.DCP import main as DCP
 from image_preprocessing.CLAHE import main as CLAHE
 from image_preprocessing.GBdehazingRCorrection import main as GBRC
@@ -11,9 +12,50 @@ from image_preprocessing.GC import main as GC
 # This file will contain all of the different image processing algorithms
 # Each algorithm should have its own separate function
 
+def main():
+    parser = argparse.ArgumentParser(description='Image Preprocessing')
+    parser.add_argument('--functions', type=str, help='An optional argument to specify which image preprocessing functions to run (comma-seperated). If not specified, all functions will be run. Ex: "-- functions amine_rhone,clahe,gc"')
+    args = parser.parse_args()
+
+    possibles = globals().copy()
+    possibles.update(locals())
+
+    functionsStr = args.functions
+    functions = []
+    if functionsStr :
+        for function in functionsStr.split(","):
+            try:
+                functions.append(possibles.get(function))
+            except Exception:
+                print(function + " is not a function in the ImagePreprocessing class")
+                return
+    else:
+        functions = [amine_rhone, clahe, dcp, gbrc, gc]
+    
+    # Test an image processing algorithm
+    
+    imgPaths = ["test_images/frame216.jpg"]
+    # imgPaths = ["test_images/frame216.jpg", "test_images/frame220.jpg", "test_images/frame223.jpg", "test_images/frame230.jpg", "test_images/frame235.jpg", "test_images/frame242.jpg", "test_images/frame258.jpg", ]
+    imgs = []
+
+    for imgPath in imgPaths:
+        for function in functions:
+            imgs.append(function(imgPath))
+
+    before = True
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    for img, ax in zip(imgs, axes.ravel()):
+        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), interpolation="nearest")
+        ax.xaxis.set_ticks([])
+        ax.yaxis.set_ticks([])
+        ax.set_xlabel("Before" if before else "After")
+        before = not before
+    fig.tight_layout()
+    plt.show()
+    
+
 def no_preprocessing(image_path):
     return cv2.imread(image_path)
-
 
 #Credit to this research paper: http://www.lsis.org/rov3d/article/art_AmineRhone2012.html
 def amine_rhone(image_path):
@@ -82,29 +124,7 @@ def gc(image_path):
     return GC.RecoverGC(img)
 
 if __name__ == '__main__':
-    # Test an image processing algorithm
+    main()
+
     
-    imgPaths = ["test_images/frame216.jpg"]
-    # imgPaths = ["test_images/frame216.jpg", "test_images/frame220.jpg", "test_images/frame223.jpg", "test_images/frame230.jpg", "test_images/frame235.jpg", "test_images/frame242.jpg", "test_images/frame258.jpg", ]
-    functions = [amine_rhone, clahe, dcp, gbrc, gc]
-    imgs = []
-
-    for imgPath in imgPaths:
-        imgs.append(no_preprocessing(imgPath))
-        #imgs.append(amine_rhone(imgPath))
-        #imgs.append(clahe(imgPath))
-        #imgs.append(dcp(imgPath))
-        #imgs.append(gbrc(imgPath))
-        imgs.append(gc(imgPath))
-
-    before = True
-    fig, axes = plt.subplots(nrows=1, ncols=2)
-    for img, ax in zip(imgs, axes.ravel()):
-        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), interpolation="nearest")
-        ax.xaxis.set_ticks([])
-        ax.yaxis.set_ticks([])
-        ax.set_xlabel("Before" if before else "After")
-        before = not before
-    fig.tight_layout()
-    plt.show()
 
