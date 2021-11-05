156fae2cd022afcc4c0c644787c486d39ffe63a4
Vedarsh Shah <vedarsh.shah@duke.edu>
2021-11-04 20:11:51 -0400

    Added capability to test multiple images at the same time

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -15,8 +15,8 @@ from image_preprocessing_pkg.GC import main as GC
 def main():
     # Use ArgumentParser to get functions and image optional arguments
     parser = argparse.ArgumentParser(description='Image Preprocessing')
-    parser.add_argument('--functions', type=str, help='An optional argument to specify which image preprocessing functions to run (comma-seperated). If not specified, only no_preprocessing will be run. Ex: "-- functions amine_rhone,clahe,gc" NOTE: no_preprocessing is always run, regardless of the specified functions.')
-    parser.add_argument('--image', type=str, help='An optional argument to specify which image in the test_images folder should be used. If not specified, frame216.jpg will be used.')
+    parser.add_argument('--functions', type=str, help='An optional argument to specify which image preprocessing functions to run (comma-seperated). If not specified, only no_preprocessing will be run. Ex: "--functions amine_rhone,clahe,gc" NOTE: no_preprocessing is always run, regardless of the specified functions.')
+    parser.add_argument('--images', type=str, help='An optional argument to specify which images in the test_images folder should be used (comma-seperated). Ex: "--images frame220.jpg,frame223.jpg,frame230.jpg" If not specified, frame216.jpg will be used.')
     args = parser.parse_args()
     
     # Get list of functions
@@ -28,7 +28,7 @@ def main():
     # If the functions argument was not specified, functions list only contains no_preprocessing as the default
     functionsStr = args.functions
     functions = [no_preprocessing]
-    if functionsStr :
+    if functionsStr:
         for functionName in functionsStr.split(","):
             function = possibles.get(functionName)
             if function:
@@ -37,44 +37,61 @@ def main():
                 print("There is no image preprocessing function with name " + functionName + ".")
                 return
     
-    # If the image argument was specified, check if the image exists and replace the value of imgPath with it
-    # If the image does not exist, let the user know via command line and stop execution
-    # If the image argument was not specified, imgPath contains frame216.jpg as the default
-    imgPath = "test_images/frame216.jpg"
-    if args.image:
-        if exists("test_images/" + args.image):
-            imgPath = "test_images/" + args.image
-        else:
-            print("There is no file with name " + args.image + " in the test_images directory.")
-            return
+    # If the images argument was specified, check if the image exists and replace the value of imgPaths with it
+    # If one of the images does not exist, let the user know via command line and stop execution
+    # If the images argument was not specified, imgPaths contains frame216.jpg as the default
+    imgPaths = ["test_images/frame216.jpg"]
+    if args.images:
+        for img in args.images.split(","):
+            if exists("test_images/" + img):
+                imgPaths.append("test_images/" + img)
+            else:
+                print("There is no file with name " + img + " in the test_images directory.")
+                return
+        imgPaths.pop(0)
     
-    # Create subplots a fixed 3 columns and necessary number of rows to display all images
-    cols = 3 if len(functions) >=3 else len(functions)
-    rows = math.ceil(len(functions) / cols)
-    fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6), squeeze=False)
+    # Process each image and display a figure for comparison
+    for imgPath in imgPaths:
+        # Print which image is being processed
+        print("Running " + imgPath)
 
-    # Loop through all preprocessing functions that need to be executed
-    # Apply each function to the image, time its executin, and show it on the matplotlib
-    for i in range(len(functions)):
+        # Create subplots a fixed 3 columns and necessary number of rows to display all images
+        cols = 3 if len(functions) >=3 else len(functions)
+        rows = math.ceil(len(functions) / cols)
+        fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6), squeeze=False)
 
-        processingStartTime = time.time()
-        processedImg = functions[i](imgPath)
-        processingTime = time.time() - processingStartTime;
-        print(f'{functions[i].__name__:>20}' + " %.2f secs" % processingTime)
+        # Loop through all preprocessing functions that need to be executed
+        # Apply each function to the image, time its executin, and show it on the matplotlib
+        for i in range(len(functions)):
 
-        ax = axes.ravel()[i]
-        ax.imshow(cv2.cvtColor(processedImg, cv2.COLOR_BGR2RGB), interpolation="nearest")
-        ax.xaxis.set_ticks([])
-        ax.yaxis.set_ticks([])
-        ax.set_xlabel(functions[i].__name__)
+            processingStartTime = time.time()
+            processedImg = functions[i](imgPath)
+            processingTime = time.time() - processingStartTime
+            print(f'{functions[i].__name__:>20}' + " %.2f secs" % processingTime)
 
-    # Delete any subplots that do not contain an image
-    for i in range((rows * cols) - len(functions)):
-        fig.delaxes(axes[rows - 1][cols - 1 - i])
+            ax = axes.ravel()[i]
+            ax.imshow(cv2.cvtColor(processedImg, cv2.COLOR_BGR2RGB), interpolation="nearest")
+            ax.xaxis.set_ticks([])
+            ax.yaxis.set_ticks([])
+            ax.set_xlabel(functions[i].__name__)
 
-    # Show the plot with all the images
-    fig.tight_layout()
-    plt.show()
+        # Delete any subplots that do not contain an image
+        for i in range((rows * cols) - len(functions)):
+            fig.delaxes(axes[rows - 1][cols - 1 - i])
+
+        # Set the figure title to the filename of the image
+        imgPathSplit = imgPath.split("/")
+        fig.suptitle(imgPathSplit[len(imgPathSplit) - 1])
+
+        # Show the plot with all the images
+        fig.tight_layout()
+
+        # Newline for spacing
+        print()
+
+        # Show each figure
+        # User will have to close currently displayed figure to see figure of next image
+        plt.show()
     
 
 def no_preprocessing(image_path):
